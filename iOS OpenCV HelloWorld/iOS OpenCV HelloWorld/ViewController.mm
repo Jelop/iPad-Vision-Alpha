44b70ba4d780847c5aba707cf8ee95ec68af057a
Joshua La Pine <jlapine@cs.otago.ac.nz>
2015-10-01 19:53:00 +1300

    keeping the streak alive

--- iOS OpenCV HelloWorld/iOS OpenCV HelloWorld/ViewController.mm
+++ iOS OpenCV HelloWorld/iOS OpenCV HelloWorld/ViewController.mm
@@ -85,22 +85,116 @@ Mat cameraMatrix, distCoeffs;
 }
 
 #ifdef __cplusplus
+
+
+#define res_x 1920
+#define res_y 1080
+
+struct blob{
+    Point2i topleft;
+    Point2i botright;
+    int row;
+    int col;
+    int count;
+};
+
+vector<blob> blobs;
+
+-(int) findBlobs:(cv::Mat&) image{
+    
+    //cout << "in findBlobs" << endl;
+    //Mat image;
+    //image.copyTo(image);
+    // cout << "about to enter for" << endl;
+    for(int row = 0; row < image.rows; row++){
+        for(int col = 0; col < image.cols; col++){
+            if(image.at<uchar>(row,col) == 0){
+                //cout << "in if" << endl;
+                bool blob_exists = false;
+                for(int k = 0; k < blobs.size(); k++){
+                    //cout << "iterating over blob vector" << endl;
+                    if(row >= blobs[k].topleft.x && row <= blobs[k].botright.x &&
+                       col >= blobs[k].topleft.y && col <= blobs[k].botright.y){
+                        //cout << "added to existing blob" << endl;
+                        blobs[k].row += row;
+                        blobs[k].col += col;
+                        blobs[k].count++;
+                        blob_exists = true;
+                        break;
+                    }
+                }
+                
+                if(!blob_exists){
+                    
+                    // might need to make the box a square...
+                    
+                    /* if(row - 10 < 0 || col - 75 < 0 ||
+                     row + 210 > res_y || col + 125 > res_x){
+                     cout << " early termination" << endl;
+                     return -1;
+                     }*/
+                    
+                    //cout << "creating new blob" << endl;
+                    blobs.push_back(blob());
+                    int index = blobs.size()-1;
+                    
+                    blobs[index].topleft = Point2i(row - 10, col - 75);
+                    blobs[index].botright = Point2i(row + 210, col + 125);
+                    blobs[index].row = row;
+                    blobs[index].col = col;
+                    blobs[index].count = 1;
+                    std::cout << "successfully created new blob" << std::endl;
+                    
+                }
+            }
+        }
+    }
+    return 0;
+}
+
 - (void) processImage:(cv::Mat &)image{
+    
+    blobs.clear();
+    cv::Mat copy;
+    image.copyTo(copy);
+    copy.convertTo(image, CV_8U);
+    
+    threshold(image, image, 50, 255, THRESH_BINARY);
+    
+    [self findBlobs:image];
+    
+    std::cout << "before cirlce drawing" << std::endl;
+    for(int i = 0; i < blobs.size(); i++){
+        int x = blobs[i].col/blobs[i].count;
+        int y = blobs[i].row/blobs[i].count;
+        std::cout << "circle " << i << " x = " << x << std::endl;
+        std::cout << "circle " << i << " y = " << y << std::endl;
+        cv::Point center(cvRound(x), cvRound(y));
+        cv::circle(image, center, 3, cv::Scalar(255,0,0), -1, 8, 0);
+    }
+
+    
+    
+    
+    
+    
+    
+    
     
     /*[videoCaptureDevice lockForConfiguration:nil];
     [videoCaptureDevice setVideoZoomFactor:1];
     [videoCaptureDevice setFocusModeLockedWithLensPosition:0.7 completionHandler: nil];
     [videoCaptureDevice unlockForConfiguration];*/
-    NSLog([NSString stringWithFormat:@"%1.9f", videoCaptureDevice.lensPosition]);
-    vector<Point2f> pixelcorners;
+    //NSLog([NSString stringWithFormat:@"%1.9f", videoCaptureDevice.lensPosition]);
+    //vector<Point2f> pixelcorners;
     //vector<vector<Point2f> > vecpixelcorners(1);
-    vector<Point2f> imagepoints;
-    Mat rvec, tvec;
-    bool vectors = false;
-    bool patternfound = findChessboardCorners(image, boardSize, pixelcorners,
+    //vector<Point2f> imagepoints;
+    //Mat rvec, tvec;
+    //bool vectors = false;
+    /*bool patternfound = findChessboardCorners(image, boardSize, pixelcorners,
                                               CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
-                                              + CALIB_CB_FAST_CHECK);
-    drawChessboardCorners(image, boardSize, pixelcorners, patternfound);
+                                              + CALIB_CB_FAST_CHECK);*/
+    /*drawChessboardCorners(image, boardSize, pixelcorners, patternfound);*/
     
     //Mat gray_image;
     //std::cout << vecpixelcorners.size() << " " << corners.size() << std::endl;
@@ -108,12 +202,12 @@ Mat cameraMatrix, distCoeffs;
     //cv::Size imageSize = gray_image.size();
     //vecpixelcorners.push_back(pixelcorners);
     //corners.resize(vecpixelcorners.size(), corners[0]);
-    if(patternfound && !pixelcorners.empty()){
+    /*if(patternfound && !pixelcorners.empty()){
         vectors = solvePnP(corners, pixelcorners, cameraMatrix, distCoeffs, rvec, tvec, false);
         //vectors = calibrateCamera(corners, vecpixelcorners, imageSize, cameraMatrix, distCoeffs, rvec, tvec);
-    }
+    }*/
     
-    if(vectors){
+    /*if(vectors){
         //cout << "Rotation matrix: " << rvec << endl;
         //cout << "Translation matrix: " <<tvec << endl;
         projectPoints(polypoints, rvec, tvec, cameraMatrix, distCoeffs, imagepoints);
@@ -131,7 +225,7 @@ Mat cameraMatrix, distCoeffs;
         line(image, imagepoints[5], imagepoints[6], Scalar(255,0,0), 5, 8);
         line(image, imagepoints[6], imagepoints[7], Scalar(255,0,0), 5, 8);
         line(image, imagepoints[7], imagepoints[4], Scalar(255,0,0), 5, 8);
-    }
+    }*/
 
 }
 #endif
